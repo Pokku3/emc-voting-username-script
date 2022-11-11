68ef08c74a8379a4746ac30d599f0a5aea07e421
Pokku3 <Pokku3@users.noreply.github.com>
2022-11-11 21:05:07 +0100

    Update URL and input field matching for TopG.org

--- userscript.js
+++ userscript.js
@@ -1,15 +1,15 @@
 // ==UserScript==
  // @name        Autofill username in EMC vote forms
- // @version      1.04
+ // @version      1.05
  // @description  Autofill your Minecraft username in the forms of websites when voting for Empire Minecraft
  // @author      Pokku8
  // By class:
- // @match        https://topg.org/Minecraft/in-355353*
  // @match        https://minecraft-server.net/vote/Aikar/
  // @match        https://minecraftservers100.com/vote/112*
  // @match        https://minecraftservers.biz/empire-minecraft/
  // @match        https://minecraftservers.org/vote/134885
  // By name:
+ // @match        https://topg.org/minecraft-servers/server-355353*
  // @match        https://www.planetminecraft.com/server/empire-minecraft-2906938/vote/
  // @match        https://minecraft-server-list.com/server/240530/vote/
  // @grant        none
@@ -18,7 +18,7 @@
  (function() {
      'use strict';
      // Fill username
-     var usernameForm = document.querySelectorAll('.form-control, .sm-form-control, .form-input, [name="mcname"], [name="ignn"]');
+     var usernameForm = document.querySelectorAll('.form-control, .sm-form-control, .form-input, [name="mcname"], [name="ignn"], [name="game_user"]');
      for (var i = 0; i < usernameForm.length; i++) {
          usernameForm[i].value = 'YOURNAME'; // <--- CHANGE YOUR NAME
      }
