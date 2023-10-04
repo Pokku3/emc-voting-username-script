2abd5c3d482732d85c2e9f3120396329bc15c0d3
Pokku3 <Pokku3@users.noreply.github.com>
2023-10-04 21:29:20 +0200

    Update input field matching for MinecraftServers.org

--- userscript.js
+++ userscript.js
@@ -1,24 +1,24 @@
 // ==UserScript==
 // @name         Autofill username in EMC vote forms
-// @version      1.05
+// @version      1.06
 // @description  Autofill your Minecraft username in the forms of websites when voting for Empire Minecraft
 // @author       Pokku8
 // By class:
 // @match        https://minecraft-server.net/vote/Aikar/
 // @match        https://minecraftservers100.com/vote/112*
 // @match        https://minecraftservers.biz/empire-minecraft/
-// @match        https://minecraftservers.org/vote/134885
 // By name:
 // @match        https://topg.org/minecraft-servers/server-355353*
 // @match        https://www.planetminecraft.com/server/empire-minecraft-2906938/vote/
 // @match        https://minecraft-server-list.com/server/240530/vote/
+// @match        https://minecraftservers.org/vote/134885
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     // Fill username
-    var usernameForm = document.querySelectorAll('.form-control, .sm-form-control, .form-input, [name="mcname"], [name="ignn"], [name="game_user"]');
+    var usernameForm = document.querySelectorAll('.form-control, .sm-form-control, .form-input, [name="mcname"], [name="ignn"], [name="game_user"], [name="username"]');
     for (var i = 0; i < usernameForm.length; i++) {
         usernameForm[i].value = 'YOURNAME'; // <--- CHANGE YOUR NAME
     }
