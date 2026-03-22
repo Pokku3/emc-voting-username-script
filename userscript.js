1a1e2a3148730af7110aeb5e147f893241a3ae5a
Pokku3 <Pokku3@users.noreply.github.com>
2026-03-22 22:14:31 +0100

    Update server list
    - Add Minecraft-Mp.com (old, for completeness)
    - Add Minestatus.net (new)
    - Remove no longer existing servers and corresponding class queries

--- userscript.js
+++ userscript.js
@@ -1,28 +1,23 @@
 // ==UserScript==
 // @name         Autofill username in EMC vote forms
-// @version      1.07
+// @version      1.08
 // @description  Autofill your Minecraft username in the forms of websites when voting for Empire Minecraft
 // @author       Pokku8
-// By class:
-// @match        https://minecraft-server.net/vote/Aikar/
-// @match        https://minecraftservers100.com/vote/112*
-// @match        https://minecraftservers.biz/empire-minecraft/
 // By name:
 // @match        https://topg.org/minecraft-servers/server-355353*
-// @match        https://www.planetminecraft.com/server/empire-minecraft-2906938/vote/
+// @match        https://minecraft-mp.com/server/4659/vote/
+// @match        https://minestatus.net/server/vote/play.emc.gs
 // @match        https://minecraft-server-list.com/server/240530/vote/
-// @match        https://minecraftservers.org/vote/134885
+// @match        https://minecraftservers.org/vote/2
+// @match        https://www.planetminecraft.com/server/empire-minecraft-2906938/vote/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     // Fill username
-    var usernameForm = document.querySelectorAll('.form-control, .sm-form-control, .form-input, [name="mcname"], [name="ignnn"], [name="game_user"], [name="username"]');
+    var usernameForm = document.querySelectorAll('[name="mcname"], [name="ignnn"], [name="nickname"], [name="username"]');
     for (var i = 0; i < usernameForm.length; i++) {
         usernameForm[i].value = 'YOURNAME'; // <--- CHANGE YOUR NAME
     }
-
-    // Rate 5/5 on Minecraft-Server.net
-    document.getElementById('rate-10').click();
 })();
